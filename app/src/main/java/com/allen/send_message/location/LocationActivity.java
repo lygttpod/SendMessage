69efa1b843e469ea81f130f1d359fee0056eedea
allen <lygttpod@163.com>
2016-12-02 10:25:45 +0800

    初始化项目结构

---
+++ app/src/main/java/com/allen/send_message/location/LocationActivity.java
@@ -0,0 +1,10 @@
+package com.allen.send_message.location;
+
+/**
+ * Created by allen on 2016/12/2.
+ *
+ * 定位
+ */
+
+public class LocationActivity {
+}
---
+++ app/src/main/java/com/allen/send_message/zone/ZoneActivity.java
@@ -0,0 +1,10 @@
+package com.allen.send_message.zone;
+
+/**
+ * Created by allen on 2016/12/2.
+ *
+ * 大本营
+ */
+
+public class ZoneActivity {
+}
